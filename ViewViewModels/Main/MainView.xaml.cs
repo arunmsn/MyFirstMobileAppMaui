4c955b0fa7776c0f095e7f7b79a8c86bdf01d860
Arun Sathia <arunmsn19@gmail.com>
2023-09-13 16:08:38 -0500

    Entered message using StackLayout in MainView

--- ViewViewModels/Main/MainView.xaml.cs
+++ ViewViewModels/Main/MainView.xaml.cs
@@ -1,8 +1,8 @@
 namespace MyFirstMobileApp.ViewViewModels.Main;
 
-public partial class NewPage1 : ContentPage
+public partial class MainView : ContentPage
 {
-	public NewPage1()
+	public MainView()
 	{
 		InitializeComponent();
 	}
