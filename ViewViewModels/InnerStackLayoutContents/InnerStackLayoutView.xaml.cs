bb7d190ef2a45fe15486d81b4867f17cc4cee379
Arun Sathia <arunmsn19@gmail.com>
2023-10-04 15:22:22 -0500

    Added in InnerStackLayoutContents, with View, ViewModel, and InnerTitleLayout

---
+++ ViewViewModels/InnerStackLayoutContents/InnerStackLayoutView.xaml.cs
@@ -0,0 +1,10 @@
+namespace MyFirstMobileApp.ViewViewModels.InnerStackLayoutContents;
+
+public partial class InnerStackLayoutView : ContentPage
+{
+	public InnerStackLayoutView()
+	{
+		InitializeComponent();
+		BindingContext = new InnerStackLayoutViewModel();
+	}
+}
---
+++ ViewViewModels/InnerStackLayoutContents/InnerStackLayoutViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MyFirstMobileApp.Models;
+using MyFirstMobileApp.ViewViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstMobileApp.ViewViewModels.InnerStackLayoutContents
+{
+    public class InnerStackLayoutViewModel : BaseViewModel
+    {
+        public string InnerTitle { get; set; } = InnerTitleLayout.innerPageTitle;
+
+        public InnerStackLayoutViewModel()
+        {
+            Title = TitleLayout.PageTitle;
+        }
+    }
+}
