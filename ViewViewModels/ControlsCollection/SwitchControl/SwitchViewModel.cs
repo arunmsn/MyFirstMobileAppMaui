5021c3d2a3bc215071b7a7b66824d85a362e6edf
SwordCraft-Hero <arunmsn19@gmail.com>
2023-11-27 21:09:37 -0600

    Fixed Switch, Stepper, and Slider.

--- ViewViewModels/ControlsCollection/SwitchControl/SwitchViewModel.cs
+++ ViewViewModels/ControlsCollection/SwitchControl/SwitchViewModel.cs
@@ -3,6 +3,7 @@ using MyFirstMobileApp.ViewViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,9 @@ namespace MyFirstMobileApp.ViewViewModels.ControlsCollection.SwitchControl
 {
     public class SwitchViewModel : BaseViewModel
     {
+        public bool Label1 { get; set; } = true;
+        public bool Label2 { get; set; } = false;
+        
         public SwitchViewModel() 
         {
 
